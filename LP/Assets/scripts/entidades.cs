2501463936c636e85f91ba907d04beb9add54c1c
RicardoMenezesBandeira <ricardobandeira197@gmail.com>
2022-11-22 21:44:35 -0300

    v0.0.01

---
+++ LP/Assets/scripts/entidades.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class entidades : MonoBehaviour
+{
+
+    private int HP;
+    private int DEF;
+    private int ATK;
+    private habilidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
